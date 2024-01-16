d20b1bfee43e1b0a481c99c0d958b5148cacc738
EM1100 <eftikamanika@gmail.com>
2024-01-16 11:36:56 +0100

    My first commit

---
+++ src/app/customers/customer.component.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  ValidatorFn,
+  FormControlOptions,
+} from '@angular/forms';
+
+import { Customer } from './customer';
+
+function emailMatcher(
+  c: AbstractControl | any
+): { [key: string]: boolean } | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  }
+  return { match: true };
+}
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (
+      c.value !== null &&
+      (isNaN(c.value) || c.value < min || c.value > max)
+    ) {
+      return { range: true };
+    }
+    return null;
+  };
+}
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css'],
+})
+export class CustomerComponent implements OnInit {
+  customerForm!: FormGroup;
+  customer = new Customer();
+  emailMessage!: string;
+
+  private validationMessages = {
+    required: 'Please enter your email address.',
+    email: 'Please enter a valid email address.',
+  };
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.fb.group(
+        {
+          email: ['', [Validators.required, Validators.email]],
+          confirmEmail: ['', Validators.required],
+        },
+        { validator: emailMatcher } as FormControlOptions
+      ),
+
+      phone: '',
+      notification: 'email',
+      rating: [null, ratingRange(1, 5)],
+      sendCatalog: true,
+    });
+
+    this.customerForm
+      .get('notification')
+      ?.valueChanges.subscribe((value) => this.setNotification(value));
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl?.valueChanges.subscribe((value) =>
+      this.setMessage(emailControl)
+    );
+  }
+  setMessage(emailControl: AbstractControl<any, any>): void {
+    throw new Error('Method not implemented.');
+  }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      email: 'jack@torchwood.com',
+      sendCatalog: false,
+    });
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl?.setValidators(Validators.required);
+    } else {
+      phoneControl?.clearValidators();
+    }
+    phoneControl?.updateValueAndValidity();
+  }
+
+  log() {
+    console.log(
+      'my log ',
+      this.customerForm.value,
+      this.customerForm.get('emailGroup')?.errors ||
+        ((this.customerForm.get('emailGroup.confirmEmail')?.touched ||
+          this.customerForm.get('emailGroup.confirmEmail')?.dirty) &&
+          !this.customerForm.get('emailGroup.confirmEmail')?.valid)
+    );
+  }
+}
